bcae5042525b4fa8e84610408b9a7b27f931aa3b
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-30 18:12:13 +1200

    revert

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,13 @@
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-import serverless from 'serverless-http'
-
-import express from 'express'
-import todos from './routes/todos.ts'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
-const server = express()
+import express from 'express'
+import todos from './routes/todos.ts'
 
+const server = express()
 server.use(express.json())
 server.use(express.static(Path.join(__dirname, 'public')))
 
@@ -24,9 +22,9 @@ if (process.env.NODE_ENV === 'production') {
   console.log(__dirname)
   server.use(express.static(Path.join(__dirname, '../client')))
 
-  server.get('*', (req, res) => {
+  server.get('/', (req, res) => {
     res.sendFile(Path.join(__dirname, 'index.html'))
   })
 }
 
-export const handler = serverless(server)
+export default server
